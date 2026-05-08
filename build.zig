8687a24743b0e6732389418d75baed108783ab16
fluffy-melli <kimjaehoon5142@gmail.com>
2026-05-09 01:54:56 +0900

    remove debug symbols

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{
-        .preferred_optimize_mode = .ReleaseSafe,
+        .preferred_optimize_mode = .ReleaseSmall,
     });
 
     const raylib_dep = b.dependency("raylib_zig", .{
@@ -57,6 +57,7 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
+            .strip = true,
         }),
     });
 
